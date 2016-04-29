02a7db75424380a38792572129b7b122550f8772
Jeff Konnen <jeff.konnen@act.etat.lu>
2016-04-29 22:41:10 +0200

    introduced grouping concept to display average delay in minutes per station and use a weight attribute which shows the gravity of the delay from 0 to 1 where 1 is 10 minutes

--- liveBusDelayHeatMap/livebusdelayheatmap/__init__.py
+++ liveBusDelayHeatMap/livebusdelayheatmap/__init__.py
@@ -1,5 +1,6 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
+from sqlalchemy import text
 
 from .models import (
     DBSession,
@@ -20,6 +21,22 @@ def main(global_config, **settings):
         Base.metadata.create_all(engine)
     except:
         pass
+    try:
+        # drop the newly created tables and replace them by views (did not find how to do this otherwise, but works)
+        s1 = text("""drop table v_bus_average_delays_by_line;
+                    create view v_bus_average_delays_by_line as
+                    select id,time,geom,name,line,avg(delay) as mean_delay, avg(delay)/ 10 as weight from busdelays
+                    group by (id,time,geom,name,line);""")
+        s2 = text("""drop table v_bus_average_delays;
+                    create view v_bus_average_delays as
+                    select id,time,geom,name,avg(delay) as mean_delay, avg(delay)/ 10 as weight from busdelays
+                    group by (id,time,geom,name);""")
+        c = engine.connect()
+        c.execute(s1)
+        c.execute(s2)
+    except:
+        pass
+
     
     config.add_renderer('geojson', GeoJSON())
     config.include('pyramid_chameleon')
